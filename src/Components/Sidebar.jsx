509a2839f4823896934da83cf6d401b1261cdb3d
AadeshGupta10 <106733380+AadeshGupta10@users.noreply.github.com>
2025-02-02 16:51:41 +0530

    Place the Static Article Data in Constant File

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,48 +1,10 @@
 import { useState } from 'react'
-import { Link, Outlet } from 'react-router-dom'
 import Articleheader from './Articleheader'
+import { article_array } from '../constant';
 
 const Sidebar = () => {
 
   const [currentId, setCurrentId] = useState(0);
-
-  const article_array = [
-    {
-      title: "Objective",
-      data: [
-        "To undertake, promote, guide, coordinate and aid research and development in the field of remote sensing.",
-        "To provide consultancy services and arrange airborne survey facilities to user agencies on actual costs basis."
-      ]
-    },
-    {
-      title: "Implementation",
-      data: [
-        "To carry out surveys for monitoring and assessment of the entire gamut of natural resources using remote sensing techniques.",
-        "To carry out special temporal surveys to monitor changing land-use patterns, environmental changes, irrigation systems, forest resources, and crop disease surveillance."
-      ]
-    },
-    {
-      title: "Approach",
-      data: [
-        "To develop efficient data acquisition and retrieval system and to act as repository of various natural resources data.",
-        "To act as a nodal organisation in the State advising the user agencies and disseminate the remote sensing technology at operational levels."
-      ]
-    },
-    {
-      title: "Sphere of Activity",
-      data: [
-        "To provide research and development support to the teaching and research organisations of the State in specified areas of remote sensing technology.",
-        "To carry out field investigations connected with the activities of remote sensing remote sensing technology and its applications."
-      ]
-    },
-    {
-      title: "Mobile Apps",
-      data: [
-        "To cooperate and collaborate with other national and international organisations in the field of remote sensing and disciplines.",
-        "To periodically publish the results of remote sensing investigations carried out by the Centre."
-      ]
-    },
-  ]
 
   return (
     <div className='w-screen min-h-screen bg-[#F6F6F9]'>
@@ -75,10 +37,10 @@ const Sidebar = () => {
           <Articleheader title={article_array[currentId].title} />
           <div className="list flex flex-col gap-5">
             {
-              article_array[currentId].data.map((content,id)=>
-              <p key={"content_"+id}>
-                {content}
-              </p>
+              article_array[currentId].data.map((content, id) =>
+                <p key={"content_" + id}>
+                  {content}
+                </p>
               )
             }
           </div>
---
+++ src/constant/index.js
@@ -0,0 +1,37 @@
+export const article_array = [
+    {
+        title: "Objective",
+        data: [
+            "To undertake, promote, guide, coordinate and aid research and development in the field of remote sensing.",
+            "To provide consultancy services and arrange airborne survey facilities to user agencies on actual costs basis."
+        ]
+    },
+    {
+        title: "Implementation",
+        data: [
+            "To carry out surveys for monitoring and assessment of the entire gamut of natural resources using remote sensing techniques.",
+            "To carry out special temporal surveys to monitor changing land-use patterns, environmental changes, irrigation systems, forest resources, and crop disease surveillance."
+        ]
+    },
+    {
+        title: "Approach",
+        data: [
+            "To develop efficient data acquisition and retrieval system and to act as repository of various natural resources data.",
+            "To act as a nodal organisation in the State advising the user agencies and disseminate the remote sensing technology at operational levels."
+        ]
+    },
+    {
+        title: "Sphere of Activity",
+        data: [
+            "To provide research and development support to the teaching and research organisations of the State in specified areas of remote sensing technology.",
+            "To carry out field investigations connected with the activities of remote sensing remote sensing technology and its applications."
+        ]
+    },
+    {
+        title: "Mobile Apps",
+        data: [
+            "To cooperate and collaborate with other national and international organisations in the field of remote sensing and disciplines.",
+            "To periodically publish the results of remote sensing investigations carried out by the Centre."
+        ]
+    },
+]
